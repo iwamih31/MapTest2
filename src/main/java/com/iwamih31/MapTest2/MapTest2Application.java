e335f77af099b80c6515259c7119aa662a49514b
iwamih31 <iwamih31@gmail.com>
2024-05-23 11:31:53 +0900

    first commit

---
+++ src/main/java/com/iwamih31/MapTest2/MapTest2Application.java
@@ -0,0 +1,13 @@
+package com.iwamih31.MapTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iwamih31/MapTest2/MapTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwamih31.MapTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
